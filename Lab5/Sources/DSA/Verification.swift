797d0f58d25fa49875938b6d8fba1963f46955b9
Artyom Rudakov <two_dollars@icloud.com>
2020-05-17 21:14:15 +0300

    Implements verifying

--- Lab5/Sources/DSA/Verification.swift
+++ Lab5/Sources/DSA/Verification.swift
@@ -4,3 +4,20 @@
 //
 
 import BigInt
+
+public func verify(
+    _ message: String,
+    using parameters: DSAParameters,
+    publicKey: Key,
+    signature: Signature
+) -> Bool {
+    let (q, p, g) = parameters.qpg, (r, s) = signature
+    let H = message.sha1Hash, y = publicKey
+    
+    let w = s.inverse(q)!
+    let u1 = (H * w) % q, u2 = (r * w) % q
+    let v1 = g.power(u1, modulus: p), v2 = y.power(u2, modulus: p)
+    let v = v1 * v2
+    
+    return v == r
+}
