6d0ff2a219d64b4ce4d8934b6ff9528c6e43f494
Artyom Rudakov <two_dollars@icloud.com>
2020-05-19 11:01:55 +0300

    Foundation for GUI-Playground

---
+++ DSA Playground/DSA Playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DSA Playground
+//
+//  Created by Artyom Rudakov on 19.05.2020.
+//  Copyright © 2020 Artyom Rudakov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
